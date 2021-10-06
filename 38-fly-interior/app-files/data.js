a4d1cfec22859720c6466f30ed9371ba1fb045b2
Yacht Design Works <31591598+YachtDesignWorks@users.noreply.github.com>
2021-10-06 16:37:01 -0400

    38 Interior MY

---
+++ 38-fly-interior/app-files/data.js
@@ -0,0 +1,281 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-hallway",
+      "name": "Hallway",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.5505707647791596,
+        "pitch": 0.36802095599022167,
+        "fov": 1.4247463008564816
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.939373849306259,
+          "pitch": 0.45321966890286447,
+          "rotation": 0,
+          "target": "2-head"
+        },
+        {
+          "yaw": -0.1174567345455717,
+          "pitch": 0.4553731967193446,
+          "rotation": 0,
+          "target": "3-shower"
+        },
+        {
+          "yaw": -1.0973170916660848,
+          "pitch": 0.6784432520866801,
+          "rotation": 5.497787143782138,
+          "target": "4-mid-berth"
+        },
+        {
+          "yaw": 1.5557818874325022,
+          "pitch": 0.35743654269233716,
+          "rotation": 0,
+          "target": "1-fwd-berth"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-fwd-berth",
+      "name": "Fwd Berth",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.735827940879183,
+        "pitch": 0.27892043149170576,
+        "fov": 1.4247463008564816
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9165770990294524,
+          "pitch": 0.1389616300987342,
+          "rotation": 6.283185307179586,
+          "target": "0-hallway"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-head",
+      "name": "Head",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.634929709105915,
+        "pitch": 0.7257406928632015,
+        "fov": 1.4247463008564816
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1658744192240853,
+          "pitch": 1.0326699304313394,
+          "rotation": 3.141592653589793,
+          "target": "0-hallway"
+        },
+        {
+          "yaw": -2.2295464034056582,
+          "pitch": 0.19538255401670668,
+          "rotation": 0,
+          "target": "3-shower"
+        },
+        {
+          "yaw": -2.561272647557397,
+          "pitch": 0.3238871940167378,
+          "rotation": 4.71238898038469,
+          "target": "4-mid-berth"
+        },
+        {
+          "yaw": -1.664567839536332,
+          "pitch": 0.35129785242015643,
+          "rotation": 1.5707963267948966,
+          "target": "1-fwd-berth"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-shower",
+      "name": "Shower",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.6686790069935151,
+        "pitch": 0.6468250391456181,
+        "fov": 1.4247463008564816
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0260579507240077,
+          "pitch": 0.35646082639133425,
+          "rotation": 0,
+          "target": "2-head"
+        },
+        {
+          "yaw": -1.0499254220822571,
+          "pitch": 1.1385880240752098,
+          "rotation": 9.42477796076938,
+          "target": "0-hallway"
+        },
+        {
+          "yaw": 0.320173436481463,
+          "pitch": 0.9042006508611955,
+          "rotation": 0,
+          "target": "4-mid-berth"
+        },
+        {
+          "yaw": -1.8596651431406936,
+          "pitch": 0.3986064755028451,
+          "rotation": 4.71238898038469,
+          "target": "1-fwd-berth"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-mid-berth",
+      "name": "Mid Berth",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.3851735463332773,
+        "pitch": 0.3089325588657097,
+        "fov": 1.4247463008564816
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5513705756992664,
+          "pitch": 0.3266435436662931,
+          "rotation": 3.141592653589793,
+          "target": "0-hallway"
+        },
+        {
+          "yaw": -0.6034684707599389,
+          "pitch": -0.12706399628115506,
+          "rotation": 0,
+          "target": "1-fwd-berth"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "38 FLY INTERIOR",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
